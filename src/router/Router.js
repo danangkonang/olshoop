3d01cbc2789616800670a0146a18d448e5f9dbef
danangkonang <danang@localhost.localdomain>
2020-02-11 09:46:00 +0700

    psg

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import {
-   BrowserRouter as Router,
-   Switch,
-   Route
- } from "react-router-dom";
- import Home from '../screen/Home'
- export default class App extends React.Component {
+// import React from 'react';
+// import {
+//    BrowserRouter as Router,
+//    Switch,
+//    Route
+//  } from "react-router-dom";
+//  import Home from '../screen/Home'
+//  export default class App extends React.Component {
    
-   render() {
-      return (
-         <Router>
-            <Switch>
-               <Route path="/" exact component={Home}/>
-            </Switch>
-         </Router>
-      )
-   }
+//    render() {
+//       return (
+//          <Router>
+//             <Switch>
+//                <Route path="/" exact component={Home}/>
+//             </Switch>
+//          </Router>
+//       )
+//    }
+// }
+
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>router danang konang</h1>
+    </div>
+  );
 }
+
+export default App;
